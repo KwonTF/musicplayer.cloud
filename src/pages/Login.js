51f67f0f7b2387c30e6dd67f4606df56bd0c59d1
KwonTF <ktf1008@naver.com>
2020-05-23 20:50:18 +0900

    :art: Design Login Page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Grid, Box, Typography } from '@material-ui/core';
 
-const Login = () => <>Login</>;
+const LoginGrid = styled(Grid)({
+  display: 'flex',
+  width: '100%',
+  height: '100%',
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: '#BBBBBB',
+});
+
+const LoginBox = styled(Box)({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  backgroundColor: '#DDDDDD',
+});
+
+const LoginHeader = styled(Typography)({
+  color: '#111111',
+  fontWeight: 700,
+  fontSize: '2rem',
+  letterSpacing: '2px',
+});
+const Login = () => (
+  <LoginGrid>
+    <LoginBox boxShadow={2}>
+      <LoginHeader>ESC</LoginHeader>
+      <p>AAA</p>
+    </LoginBox>
+  </LoginGrid>
+);
 export default Login;
