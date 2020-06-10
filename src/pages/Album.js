f40da368571c8a486f5d572daa06339fae0707bf
Seungho Kim <niceb5y@gmail.com>
2020-06-10 23:30:39 +0900

    :hankey: GraphQL demo

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
+import MusicViewer from '../components/MusicViewer';
 
 const ALBUM_QUERY = gql`
   {
     albums {
+      albumId
       title
+      artist
+      cover
+      tracks {
+        trackId
+        title
+        trackNumber
+        artist
+        url
+      }
     }
   }
 `;
 
 const Album = () => {
   const { loading, data } = useQuery(ALBUM_QUERY);
-  console.log(data);
+
+  const musics = data
+    ? data.albums
+        .map((album) =>
+          album.tracks.map((track) => ({
+            musicId: track.trackId,
+            title: track.title,
+            artist: track.artist,
+            album: album.title,
+            imageLink: album.cover,
+            audioLink: track.url,
+          })),
+        )
+        .reduce(
+          (accumulator, currentValue) => accumulator.concat(currentValue),
+          [],
+        )
+    : [];
+  console.log(musics);
+
   if (loading) return 'Loading...';
-  return (
-    <div>
-      {data.albums.map((album) => (
-        <p key={album.title}>{album.title}</p>
-      ))}
-    </div>
-  );
+  return <MusicViewer musics={musics} />;
 };
 
 export default Album;
